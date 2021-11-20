204f10ec95eed381241a2710352fbd54a1b012ec
alvnavra <alvnavra@gmail.com>
2021-11-20 08:43:03 +0100

    Reminder on setTimeout

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -21,7 +21,9 @@ const Search = () => {
       });
       setResults(data.query.search);
     };
-    if (term) search();
+    const timeoutId = setTimeout(() => {
+      if (term) search();
+    }, 500);
   }, [term]);
 
   const renderedResults = results.map((result) => {
